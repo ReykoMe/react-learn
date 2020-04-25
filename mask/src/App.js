781aabd06c16eea2176a97a1ff51d9b4617e30ce
ReykoMe <62741259+ReykoMe@users.noreply.github.com>
2020-04-25 22:45:08 +0300

    Added Navmenu.jsx

--- mask/src/App.js
+++ mask/src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
+
 import './App.css';
+import Navmenu from './navmenu/Navmenu';
 
 const App = () => {
   return (
-    <div className="App">
+    <div className='App'>
+       <Navmenu />
       <h3>Hello</h3>
       <p>It's my first react application</p>
     </div>
---
+++ mask/src/navmenu/Navmenu.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import './navmenu.modules.css'
+
+const Navmenu = () => {
+   return (
+   <nav className = "navigation">
+        <div>First</div>
+        <div>Second</div>
+        <div>Third</div>
+    </nav>
+)
+};
+
+export default Navmenu;
