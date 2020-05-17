26cb1cbf5907ce624f4ab878b42a52fa5b1ff3c8
ReykoMe <62741259+ReykoMe@users.noreply.github.com>
2020-05-17 20:53:36 +0300

    Main changes

--- mask/src/Messenger/Chats/users.jsx
+++ mask/src/Messenger/Chats/users.jsx
@@ -8,7 +8,7 @@ const UserItem = (props) => {
     let path = "/Messenger/" + props.id; //создаем переменную для более короткой записи в навлинке
     return (
         <div className={classes.item + ' ' + classes.selected}>
-            <NavLink to={path}>{props.name}</NavLink>
+            <NavLink to={ path }>{ props.name }</NavLink>
         </div>
     )
 }
@@ -16,8 +16,8 @@ const UserItem = (props) => {
 const Users = (props) => {
     //создаем новый массив, где usersData берет данные из массива объектов, расположенном в index.js, передаваемый через props
     let usersElementsArray = props.users.map((user) => <UserItem name={user.name}
-                                                                        id={user.id}
-                                                                        key ={user.id}/>);
+                                                                 id={user.id}
+                                                                 key ={user.id}/>);
 
     return (
         <div className="users">
