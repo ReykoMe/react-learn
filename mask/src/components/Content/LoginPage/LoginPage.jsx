735777492daf29f7285ae44d10b2f5f18812002a
ReykoMe <62741259+ReykoMe@users.noreply.github.com>
2020-06-12 22:42:33 +0300

    22:42 12.06

--- mask/src/components/Content/LoginPage/LoginPage.jsx
+++ mask/src/components/Content/LoginPage/LoginPage.jsx
@@ -1,24 +1,80 @@
-import React from 'react';
+import React from "react";
+import { useFormik } from "formik";
 const LoginForm = (props) => {
-   return (
-    <form>
-    <div class="form-group">
-      <label for="emailInput">Введите свой электронный адрес</label>
-      <input type="email" class="form-control" id="emailInput" aria-describedby="emailHelp" placeholder="Введите электронную почту" />
-      <small id="emailHelp" class="form-text text-muted">Никогда и никому не сообщеайте эти данные</small>
-    </div>
-    <div class="form-group">
-      <label for="passwordInput">Пароль</label>
-      <input type="password" class="form-control" id="passwordInput" placeholder="Введите пароль" />
-    </div>
-    <div class="form-check">
-      <input type="checkbox" class="form-check-input" id="logoutTimeout" />
-      <label class="form-check-label" for="logoutTimeout">Не выходить</label>
-    </div>
-    <button type="submit" class="btn btn-primary">Submit</button>
-  </form>
-   )
-}
+    const formik = useFormik({
+        initialValues: {
+            email: "mail@mail.com",
+            password: "",
+            stayLogined: false,
+            showPassword: false,
+        },
+        onSubmit: values => {
+          console.log(JSON.stringify(values, null, 4))
+        }
+    });
+
+    return (
+        <form onSubmit={formik.handleSubmit}>
+            <div className='form-group'>
+                <label htmlFor='email'>Введите свой электронный адрес</label>
+                <input
+                    type='email'
+                    id='email'
+                    name='email'
+                    className='form-control'
+                    aria-describedby='emailHelp'
+                    placeholder='Введите электронную почту'
+                    value={formik.values.email}
+                    onChange={formik.handleChange}
+                />
+                <small id='emailHelp' className='form-text text-muted'>
+                    Никогда и никому не сообщеайте эти данные
+                </small>
+            </div>
+            <div className='form-group'>
+                <label htmlFor='password'>Пароль</label>
+                <input
+                    type={!formik.values.showPassword ? "password" : "text"}
+                    className='form-control'
+                    id='password'
+                    name='password'
+                    placeholder='Введите пароль'
+                    value={formik.values.password}
+                    onChange={formik.handleChange}
+                />
+            </div>
+            <div className='form-check'>
+                <input
+                    type='checkbox'
+                    className='form-check-input'
+                    id='showPassword'
+                    name='showPassword'
+                    value={formik.values.showPassword}
+                    onChange={formik.handleChange}
+                />
+                <label className='form-check-label' htmlFor='showPassword'>
+                    Показать пароль
+                </label>
+            </div>
+            <div className='form-check'>
+                <input
+                    type='checkbox'
+                    className='form-check-input'
+                    id='stayLogined'
+                    name='stayLogined'
+                    value={formik.values.stayLogined}
+                    onChange={formik.handleChange}
+                />
+                <label className='form-check-label' htmlFor='stayLogined'>
+                    Не выходить
+                </label>
+            </div>
+            <button type='submit' className='btn btn-primary'>
+                Submit
+            </button>
+        </form>
+    );
+};
 
 const LoginPage = (props) => {
     return (
@@ -26,7 +82,7 @@ const LoginPage = (props) => {
             <h1>It's a Login Page</h1>
             <LoginForm />
         </div>
-    )
-}
+    );
+};
 
-export default LoginPage
+export default LoginPage;
