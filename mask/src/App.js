f436793b16f929b7f0fbd80b78a6a1b971a38a97
reykome <andrbrd@gmail.com>
2020-06-12 18:22:29 +0300

    Commit test

--- mask/src/App.js
+++ mask/src/App.js
@@ -1,30 +1,30 @@
-import React from 'react';
-import './App.css';
-import Footer from './components/Footer/Footer';
+import React from "react";
+import "./App.css";
+import Footer from "./components/Footer/Footer";
 import SidebarContainer from "./containers/SidebarContainer";
 import Messenger from "./components/Messenger/Messenger";
-import {Route} from "react-router-dom";
+import { Route } from "react-router-dom";
 import Friends from "./components/Content/FriendList/Friends";
-import ContentContainer from './containers/ContentContainer'
-import HeaderContainer from './containers/HeaderContainer';
-import LoginPage from './components/Content/LoginPage/LoginPage'
+import ContentContainer from "./containers/ContentContainer";
+import HeaderContainer from "./containers/HeaderContainer";
+import LoginPage from "./components/Content/LoginPage/LoginPage";
 const App = (props) => {
     return (
         <div className='container'>
             <HeaderContainer />
-            <div className="row">
-                <SidebarContainer store={props.store}/>
-                <div className="col-md-9 content">
+            <div className='row'>
+                <SidebarContainer store={props.store} />
+                <div className='col-md-9 content'>
                     {/*Добавляем Route (необходимо установить из npm react-router-dom), с помощью которого реализуется логика "Если в строке браузера отображается /путь условный => отображать компоненту*/}
-                    <Route path="/profile/:userId?" render={() => <ContentContainer/>}/>
-                    <Route path="/login" render={() => <LoginPage/>}/>
-                    <Route path="/messenger" render={() => <Messenger/>}/>
-                    <Route path="/friends" render={() => <Friends/>}/>
+                    <Route path='/profile/:userId?' render={() => <ContentContainer />} />
+                    <Route path='/login' render={() => <LoginPage />} />
+                    <Route path='/messenger' render={() => <Messenger />} />
+                    <Route path='/friends' render={() => <Friends />} />
                 </div>
             </div>
-            <Footer/>
+            <Footer />
         </div>
     );
-}
+};
 
 export default App;
