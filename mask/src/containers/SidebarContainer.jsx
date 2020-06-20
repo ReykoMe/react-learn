0a3f2f7f8957c653f131044fa515df851b2a3b63
ReykoMe <62741259+ReykoMe@users.noreply.github.com>
2020-06-20 18:32:45 +0300

    deleted unused files

--- mask/src/containers/SidebarContainer.jsx
+++ mask/src/containers/SidebarContainer.jsx
@@ -1,10 +1,21 @@
-import Sidebar from "../components/Sidebar/Sidebar";
+import React from "react";
 import { connect } from "react-redux";
+import Sidebar from "../components/Sidebar/Sidebar";
+import { getLastRegisteredUsers } from '../redux/reducers/sidebar-reducer';
+
+class SidebarContainer extends React.Component {
+    componentDidMount() {
+        this.props.getLastRegisteredUsers(this.props.sidebar.page, this.props.sidebar.count)
+    }
+    render() {
+        return <Sidebar {...this.props} />;
+    }
+}
 
 let mapStateToProps = (state) => {
     return {
         sidebar: state.sidebar,
     };
 };
-const SidebarContainer = connect(mapStateToProps)(Sidebar);
-export default SidebarContainer;
+
+export default connect(mapStateToProps, {getLastRegisteredUsers})(SidebarContainer);
--- mask/src/redux/reducers/sidebar-reducer.js
+++ mask/src/redux/reducers/sidebar-reducer.js
@@ -1,13 +1,29 @@
+import {friendsApi} from '../../service/api/axiosQueries'
 const initState = {
-  friends: [
-    {id: 1, firstName: "Nikola Tesla", status: "Direct Current in our Houses"},
-    {id: 2, firstName: "Dmitriy Mendeleev", status: "Let me more Vodka"},
-    {id: 3, firstName: "Isak Newton", status: "All around is a round"}
-  ]
-}
+    count: 10,
+    page: 1,
+    friends: [
+        // {id: 1, firstName: "Nikola Tesla", status: "Direct Current in our Houses"},
+        // {id: 2, firstName: "Dmitriy Mendeleev", status: "Let me more Vodka"},
+        // {id: 3, firstName: "Isak Newton", status: "All around is a round"}
+    ],
+};
 
 const sidebarReducer = (state = initState, action) => {
-  return state;
-}
+    switch (action.type) {
+        case "SET_LAST_REGISTERED_USERS":
+            return { ...state, friends: [...action.lastUsers] };
+        default:
+            return state;
+    }
+};
+
+export const setLastRegisteredUsers = (lastUsers) => ({ type: "SET_LAST_REGISTERED_USERS", lastUsers });
+export const getLastRegisteredUsers = (page, count) => (dispatch) => {
+      friendsApi.getAllUsers(page, count).then((response) => {
+        console.log(response)
+        dispatch(setLastRegisteredUsers(response.items))
+      });
+  };
 
 export default sidebarReducer;
